aaf972fb2a52da77822f04b1879f0b438d0b72c8
Jamal Ahamad <jamalahamad@Jamals-Mac.local>
2018-01-20 01:19:26 +0530

    Initial Commit

---
+++ star_pattern_logic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  star_pattern_logic
+//
+//  Created by Jamal Ahamad on 20/01/18.
+//  Copyright © 2018 Jamal Ahamad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
